75a96d99330fabe7b84668f232488794111df564
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2019-02-03 12:44:13 +0300

    оптимизация компонента Picker - убрал лишний View

--- src/screens/Settings/components/ThemePicker.js
+++ src/screens/Settings/components/ThemePicker.js
@@ -7,20 +7,18 @@ export const ThemePicker = () => {
   return (
     <StateContext.Consumer>
       {({ _changeThemeColor, themeColor, theme, styles }) => (
-        <View>
-          <Picker
-            selectedValue={themeColor}
-            style={styles.picker}
-            itemStyle={{
-              color: theme.primaryColorTxt,
-              height: 200
-            }}
-            onValueChange={color => _changeThemeColor(color)}
-          >
-            <Picker.Item label='Light' value='light' />
-            <Picker.Item label='Dark' value='dark' />
-          </Picker>
-        </View>
+        <Picker
+          selectedValue={themeColor}
+          style={styles.picker}
+          itemStyle={{
+            color: theme.primaryColorTxt,
+            height: 200
+          }}
+          onValueChange={color => _changeThemeColor(color)}
+        >
+          <Picker.Item label='Light' value='light' />
+          <Picker.Item label='Dark' value='dark' />
+        </Picker>
       )}
     </StateContext.Consumer>
   );
